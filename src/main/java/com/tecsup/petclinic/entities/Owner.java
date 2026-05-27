006a92e7a1b0ec7a2264b458bc9da9660e1619af
Edinson Jimenez <edinson.jimenez@tecsup.edu.pe>
2026-05-27 17:14:54 -0500

    Caso 2 OwnerServiceTest completado

---
+++ src/main/java/com/tecsup/petclinic/entities/Owner.java
@@ -0,0 +1,27 @@
+package com.tecsup.petclinic.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "owners")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Owner {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String address;
+
+    private String city;
+
+    private String telephone;
+}
