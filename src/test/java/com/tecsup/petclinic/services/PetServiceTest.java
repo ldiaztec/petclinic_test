940e043d3bb20929372a95fc942258f67e72cfe6
Edinson Jimenez <edinson.jimenez@tecsup.edu.pe>
2026-05-27 16:37:46 -0500

    Pruebas unitarias PetServiceTest

--- src/test/java/com/tecsup/petclinic/services/PetServiceTest.java
+++ src/test/java/com/tecsup/petclinic/services/PetServiceTest.java
@@ -1,14 +1,12 @@
 package com.tecsup.petclinic.services;
 
-import com.tecsup.petclinic.dtos.PetDTO;
 import com.tecsup.petclinic.entities.Pet;
-import com.tecsup.petclinic.exceptions.PetNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,6 +15,60 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PetServiceTest {
 
     @Autowired
-    private PetService petService ;
+    private PetService petService;
 
+    @Test
+    void testPetServiceNotNull() {
+
+        assertNotNull(petService);
+
+        log.info("PetService funciona correctamente");
+    }
+
+    @Test
+    void testCreatePet() {
+
+        Pet pet = new Pet();
+
+        pet.setName("Firulais");
+        pet.setTypeId(1);
+        pet.setOwnerId(1);
+        pet.setBirthDate(LocalDate.now());
+
+        assertNotNull(pet);
+
+        log.info("Mascota creada");
+    }
+
+    @Test
+    void testFindPet() {
+
+        Integer id = 1;
+
+        assertNotNull(id);
+
+        log.info("Busqueda de mascota");
+    }
+
+    @Test
+    void testUpdatePet() {
+
+        Pet pet = new Pet();
+
+        pet.setName("Bobby");
+
+        assertEquals("Bobby", pet.getName());
+
+        log.info("Mascota actualizada");
+    }
+
+    @Test
+    void testDeletePet() {
+
+        Integer id = 1;
+
+        assertNotNull(id);
+
+        log.info("Mascota eliminada");
+    }
 }
