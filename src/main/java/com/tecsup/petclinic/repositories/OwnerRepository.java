fdd831654779d40c87c14e1c022a50800b1bb04a
rootKarka <patrick.pajares@tecsup.edu.pe>
2026-05-27 15:40:50 -0500

    Completado Caso 2: Pruebas unitarias CRUD para OwnerServive

---
+++ src/main/java/com/tecsup/petclinic/repositories/OwnerRepository.java
@@ -0,0 +1,11 @@
+package com.tecsup.petclinic.repositories;
+
+import com.tecsup.petclinic.entities.Owner;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface OwnerRepository extends CrudRepository<Owner, Integer> {
+    List<Owner> findByLastName(String lastName);
+}
